271e234930378c005e73708e9861dedd1ac36fcb
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-07-10 15:11:50 +0100

    add ES6 module

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { BookCollection, addBook} from './modules/add_remove_Books.js';
 import  {showSection, nav} from './modules/navigation.js'
+import timeDate from './modules/time.js';
 
 const btnAdd = document.getElementById('add');
 
@@ -7,6 +8,7 @@ const bookCollection = new BookCollection();
 
 btnAdd.addEventListener('click', addBook);
 
+timeDate();
 
 window.addEventListener('load', () => {
     bookCollection.displayBooks();

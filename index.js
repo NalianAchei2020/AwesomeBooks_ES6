d7294a338aed17202e05ef7e7d6ee1784e0cd7ac
Nwanjoh Nalian Achei <88759996+NalianAchei2020@users.noreply.github.com>
2023-07-10 15:05:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import { BookCollection, addBook} from './modules/add_remove_Books.js';
 import  {showSection, nav} from './modules/navigation.js'
-import timeDate from './modules/time.js';
 
 const btnAdd = document.getElementById('add');
 
@@ -8,7 +7,6 @@ const bookCollection = new BookCollection();
 
 btnAdd.addEventListener('click', addBook);
 
-timeDate();
 
 window.addEventListener('load', () => {
     bookCollection.displayBooks();
